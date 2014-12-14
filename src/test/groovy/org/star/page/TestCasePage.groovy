fb73f474f7eab70428d575cb6260052e3d9f4345
藤江貴司 <tfujie@fujietakashi-no-MacBook-Pro.local>
2014-12-14 15:53:13 +0900

    いったんコミット

--- src/test/groovy/org/star/page/TestCasePage.groovy
+++ src/test/groovy/org/star/page/TestCasePage.groovy
@@ -4,4 +4,12 @@ package org.star.page
  * Created by Xipf on 14/12/14.
  */
 class TestCasePage {
+    static url = "testCase"
+    static at = {
+        //不変条件
+        $("h1").text() == "Test Caseリスト"
+    }
+
+    static content = {
+    }
 }
--- src/test/groovy/org/star/scenario/WhenEditTestCase.groovy
+++ src/test/groovy/org/star/scenario/WhenEditTestCase.groovy
@@ -13,25 +13,29 @@ class WhenEditTestCase extends GebReportingSpec {
         given: "トップページに移動し、管理者としてログイン"
         //タグ作成画面に移動
         to TestTagPage
-        when: "Name(タグ１)を入力"
-        and: "Description(タグ１)を入力"
-        and: "作成ボタンをクリック"
-        then: "タグ１が登録されている"
-        when: "Name(タグ２)を入力"
-        and: "Description(タグ２)を入力"
-        and: "作成ボタンをクリック"
-        then: "タグ２が登録されている"
-        when: "テストケースページに移動"
-        to TestCasePage
-        then: "テストケースページに移動されていること"
-        at TestCasePage
-        when: "Name(ケース１)を入力"
-        and: "Scenario(シナリオ１)を入力"
-        and: "Tags(タグ１、タグ２)を選択"
-        and: "作成をクリック"
-        then: "Nameにケース１が入力されているか"
-        and: "Scenarioにシナリオ１が入力されているか"
-        and: "Tagsにタグ１、タグ２が選択されているか"
+        when: "タグ作成画面にアクセス"
+        login "admin", "admin"
+        then: "タグ作成画面が表示される"
+        at TestTagPage
+//        when: "Name(タグ１)を入力"
+//        and: "Description(タグ１)を入力"
+//        and: "作成ボタンをクリック"
+//        then: "タグ１が登録されている"
+//        when: "Name(タグ２)を入力"
+//        and: "Description(タグ２)を入力"
+//        and: "作成ボタンをクリック"
+//        then: "タグ２が登録されている"
+//        when: "テストケースページに移動"
+//        to TestCasePage
+//        then: "テストケースページに移動されていること"
+//        at TestCasePage
+//        when: "Name(ケース１)を入力"
+//        and: "Scenario(シナリオ１)を入力"
+//        and: "Tags(タグ１、タグ２)を選択"
+//        and: "作成をクリック"
+//        then: "Nameにケース１が入力されているか"
+//        and: "Scenarioにシナリオ１が入力されているか"
+//        and: "Tagsにタグ１、タグ２が選択されているか"
 
 
     }
