5aa5edb8018fa6e9edf7e8d3968d98684962fc9c
takaesu0 <takaesu0@gmail.com>
2014-12-14 16:31:00 +0900

    Given/When/Then形式に変換

--- src/test/groovy/org/star/scenario/WhenNLS.groovy
+++ src/test/groovy/org/star/scenario/WhenNLS.groovy
@@ -6,20 +6,18 @@ import org.star.page.*
 
 class WhenNLS extends GebReportingSpec {
     def "アラビア語もいける"() {
-        given: "トップページにアクセスする"
+        given: "管理者としてログインする"
         to TopPage
-        when: "管理者としてログインすると"
         login "admin", "admin"
-        then: ""
-        // ログインする
-        // TestTagページにアクセスする
-        // Nameに乱数、Descriptionに"عالم مرحبا هناك"を入力する
-        // 作成をクリックする
-        // Tagリストに登録したタグが含まれるのを確認する
-        /// "Filter Me"をクリックする
-        /// Nameの"Equal To"を選択する
-        /// 値をNameに使用した乱数を設定する
-        /// "Apply"をクリックする
-        /// 追加したタグが一覧に含まれる
+        at DashBoardPage
+        when: "TestTagページにアクセスする"
+        and: "Nameに乱数、Descriptionに'عالم مرحبا هناكを入力する"
+        and: "'作成'をクリックする"
+        then: "'Tag(id:xx)を作成しました。'のメッセージが表示される"
+        when: "'Filter Me'をクリックする"
+        and: "Nameの'Equal To'を選択する"
+        and: "値をNameに使用した乱数を設定する"
+        and: "'Apply'をクリックする"
+        then: "追加したタグが一覧に含まれる"
     }
 }
